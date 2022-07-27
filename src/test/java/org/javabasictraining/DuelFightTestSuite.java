046f619fc27b10a3df653a124411eddad1056221
jdude <jdude@softserveinc.com>
2022-07-27 16:51:10 +0200

    DuelFightTestSuite - basic initial tests for main functional

--- src/test/java/org/javabasictraining/DuelFightTestSuite.java
+++ src/test/java/org/javabasictraining/DuelFightTestSuite.java
@@ -16,25 +16,12 @@ class DuelFightTestSuite {
         var dave = new Warrior();
 
         //When & Then
-        assertTrue(Battle.fight(chuck, bruce));
-        assertFalse(Battle.fight(dave, carl));
+        assertFalse(Battle.fight(chuck, bruce));
+        assertTrue(Battle.fight(dave, carl));
         assertTrue(chuck.isAlive());
         assertFalse(bruce.isAlive());
         assertTrue(carl.isAlive());
         assertFalse(dave.isAlive());
-    }
-
-    @Test
-    void whenWarriorDuelsKnight_KnightShouldWin() {
-        //Given
-        var warrior = new Warrior();
-        var knight = new Knight();
-
-        //When
-        var battleResult = Battle.fight(warrior, knight);
-
-        //Then
-        assertFalse(battleResult);
     }
 
     @Test
@@ -70,16 +57,66 @@ class DuelFightTestSuite {
     }
 
     @Test
-    @DisplayName("3. Fight: Knight should win")
-    void whenKnightFightsVsWarrior_WarriorShouldLoseTheBattle() {
+    @DisplayName("3. Fight: Warrior defends himself against knight and should fail & die")
+    void whenWarriorDefendsHimselfVsKnight_WarriorShouldFailAndDie() {
+        //Given
+        var warrior = new Warrior();
+        var knight = new Knight();
+
+        //When
+        var battleResult = Battle.fight(knight, warrior);
+        var isWarriorAlive = warrior.isAlive();
+
+        //Then
+        assertFalse(battleResult);
+        assertFalse(isWarriorAlive);
+    }
+
+    @Test
+    @DisplayName("4. Fight: Knight defends himself against warrior and should win & stay alive")
+    void whenKnightDefendsHimselfVsWarrior_KnightShouldWinAndStayAlive() {
+        //Given
+        var knight = new Knight();
+        var warrior = new Warrior();
+
+        //When
+        var battleResult = Battle.fight(warrior, knight);
+        var isKnightAlive = knight.isAlive();
+
+        //Then
+        assertTrue(battleResult);
+        assertTrue(isKnightAlive);
+    }
+
+    @Test
+    @DisplayName("5. Fight: Warrior attack knight and should fail & die")
+    void whenWarriorAttacksKnight_WarriorShouldFailAndDie() {
+        //Given
+        var warrior = new Warrior();
+        var knight = new Knight();
+
+        //When
+        var battleResult = Battle.fight(warrior, knight);
+        var isWarriorAlive = warrior.isAlive();
+
+        //Then
+        assertTrue(battleResult);
+        assertFalse(isWarriorAlive);
+    }
+
+    @Test
+    @DisplayName("6. Fight: Knight attacks warrior and should win & stay alive")
+    void whenKnightAttacksWarrior_KnightShouldWinAndStayAlive() {
         //Given
         var knight = new Knight();
         var warrior = new Warrior();
 
         //When
         var battleResult = Battle.fight(knight, warrior);
+        var isKnightAlive = knight.isAlive();
 
         //Then
         assertFalse(battleResult);
+        assertTrue(isKnightAlive);
     }
 }
