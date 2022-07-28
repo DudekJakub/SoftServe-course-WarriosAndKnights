811e1a5f612b9d67c6d83a7ca67b46a0cb81929e
Jakub Dudek <jdude@softserveinc.com>
2022-07-28 14:22:49 +0200

    Unit interface update

--- src/main/java/org/javabasictraining/encapsulatedApproach/Unit.java
+++ src/main/java/org/javabasictraining/encapsulatedApproach/Unit.java
@@ -3,12 +3,13 @@ package org.javabasictraining.encapsulatedApproach;
 public interface Unit {
 
     enum UnitType {
-        WARRIOR, KNIGHT
+        WARRIOR, KNIGHT, SUPPORT
     }
     static Unit newUnit(UnitType type) {
         return switch (type) {
             case WARRIOR -> new Warrior();
             case KNIGHT -> new Warrior(50, 7, false);
+            case SUPPORT -> new Support();
         };
     }
 
