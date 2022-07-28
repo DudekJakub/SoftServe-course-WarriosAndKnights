2adf2fb4275da2adc2d171be5a98efb676ad684b
Jakub Dudek <jdude@softserveinc.com>
2022-07-28 14:05:34 +0200

    Battle classes added for both approaches

---
+++ src/main/java/org/javabasictraining/encapsulatedApproach/Battle.java
@@ -0,0 +1,31 @@
+package org.javabasictraining.encapsulatedApproach;
+
+public class Battle {
+
+    private Battle() {}
+
+    public static boolean fight(Warrior attacker, Warrior defender) {
+        while (attacker.isAlive() && defender.isAlive()) {
+            attacker.hit(defender);
+            if (!defender.isAlive()) {
+                return defender.isAlive();
+            } else {
+                defender.hit(attacker);
+            }
+        }
+        return defender.isAlive();
+    }
+
+    public static void fight(Army attackers, Army defenders) {
+        while (!attackers.isArmyEmpty() && !defenders.isArmyEmpty()) {
+            var currentAttacker = attackers.getUnit();
+            var currentDefender = defenders.getUnit();
+
+            if (fight(currentAttacker, currentDefender)) {
+                attackers.removeDeadSolider();
+            } else {
+                defenders.removeDeadSolider();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/javabasictraining/openedApproach/Battle.java
@@ -0,0 +1,19 @@
+package org.javabasictraining.openedApproach;
+
+public class Battle {
+
+    private Battle() {}
+
+/** Code not used in this case of openedApproach (same one has been already used in encapsulatedApproach) */
+//    public static boolean DuelFight(Warrior attacker, Warrior defender) {
+//        while (attacker.isAlive() && defender.isAlive()) {
+//            attacker.hit(defender);
+//            if (!defender.isAlive()) {
+//                return defender.isAlive();
+//            } else {
+//                defender.hit(attacker);
+//            }
+//        }
+//        return defender.isAlive();
+//    }
+}
