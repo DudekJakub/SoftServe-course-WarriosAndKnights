b78d3d79c60051d2fa79e8e3082c23c6db786df1
Jakub Dudek <jdude@softserveinc.com>
2022-07-29 18:58:41 +0200

    Knight class - default constructor added #UPDATE

--- src/main/java/org/study/warriors/model/Knight.java
+++ src/main/java/org/study/warriors/model/Knight.java
@@ -1,7 +1,11 @@
 package org.study.warriors.model;
 
 public class Knight extends Warrior {
-    static final int ATTACK = 5;
+    static final int ATTACK = 7;
+
+    public Knight() {
+        super(INITIAL_HEALTH, ATTACK);
+    }
 
     @Override
     public int getAttack() {
