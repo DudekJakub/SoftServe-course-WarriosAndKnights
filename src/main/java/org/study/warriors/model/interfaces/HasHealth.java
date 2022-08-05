c70193186a6920caf821268857dd5be1db41f7bf
Jakub Dudek <jdude@softserveinc.com>
2022-08-05 13:35:30 +0200

    HasHealth interface update for healing mechanism #UPDATE

--- src/main/java/org/study/warriors/model/interfaces/HasHealth.java
+++ src/main/java/org/study/warriors/model/interfaces/HasHealth.java
@@ -5,6 +5,7 @@ public interface HasHealth {
     int getLastReceivedDamage();
     void setLastReceivedDamage(int damage);
     void reduceHealthBasedOnDamage(int damage);
+    void enlargeHealthBasedOnHeal(int heal);
     default boolean isAlive() {
         return getHealth() > 0;
     }
