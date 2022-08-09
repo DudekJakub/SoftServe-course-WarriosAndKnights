72ac2b1b8973bc6a94bbb3fde1ce7a1c6f14e77b
Jakub Dudek <jdude@softserveinc.com>
2022-08-09 21:19:35 +0200

    Battle class update/refactor for Request mechanism + for StraightFight task #UPDATE/REFACTOR

--- src/main/java/org/study/warriors/service/Battle.java
+++ src/main/java/org/study/warriors/service/Battle.java
@@ -14,12 +14,12 @@ public class Battle {
     }
 
     public static boolean fight(IWarrior attacker, IWarrior defender) {
-        LOGGER.debug("Duel between {} (AP {} | HP {}) and {} (AP {} | HP {}) begins!", attacker, attacker.getAttack(), attacker.getHealth(), defender, defender.getAttack(), defender.getHealth());
+        LOGGER.debug("Duel between {} (AP {} | HP {}) ATTACKER vs. DEFENDER {} (AP {} | HP {}) begins!", attacker, attacker.getAttack(), attacker.getHealth(), defender, defender.getAttack(), defender.getHealth());
 
         while (attacker.isAlive() && defender.isAlive()) {
             attacker.hit(defender);
             if (!defender.isAlive()) {
-                LOGGER.debug("Duel between {} (HP left {}) and {} (HP left {}) is over!", attacker, attacker.getHealth(), defender, defender.getHealth());
+                LOGGER.debug("Duel between {} (HP left {}) and {} (HP left {}) is over!", attacker.getClass().getCanonicalName(), attacker.getHealth(), defender, defender.getHealth());
                 return attacker.isAlive();
             } else {
                 defender.hit(attacker);
@@ -36,12 +36,30 @@ public class Battle {
         var it1 = attackers.firstAlive();
         var it2 = defenders.firstAlive();
 
+        LOGGER.debug("Army before battle (with HP & insertion order): " + "ATTACKERS: " + attackers.getSoldiersAndTheirHp() + " | DEFENDERS: " + defenders.getSoldiersAndTheirHp());
         LOGGER.debug("ArmyBattle has begun!");
         while (it1.hasNext() && it2.hasNext()) {
             fight(it1.next(), it2.next());
             LOGGER.debug("Alive soldiers: AttackerSide = {} | DefenderSide = {}\n", attackers.getAliveSoldiers(), defenders.getAliveSoldiers());
         }
         LOGGER.debug("ArmyBattle has ended!");
+        LOGGER.debug("Army after battle (with HP): " + "ATTACKERS: " + attackers.getSoldiersAndTheirHp() + " | DEFENDERS: " + defenders.getSoldiersAndTheirHp());
+
         return it1.hasNext();
+    }
+
+    public static boolean straightFight(Army leftArmy, Army rightArmy) {
+        LOGGER.debug("The straight fight between {} and {} has begun!", leftArmy.getSoldiersAndTheirHp(), rightArmy.getSoldiersAndTheirHp());
+        while (leftArmy.isAlive() && rightArmy.isAlive()) {
+            int smallerArmySize = Math.min(leftArmy.getArmySize(), rightArmy.getArmySize());
+            for (int i = 0; i < smallerArmySize; i++) {
+                fight(leftArmy.getSoldierFromGivenPosition(i), rightArmy.getSoldierFromGivenPosition(i));
+            }
+            LOGGER.trace("Removing dead soldiers from armies...");
+            leftArmy.removeDeadSoldiersFromArmy();
+            rightArmy.removeDeadSoldiersFromArmy();
+        }
+        LOGGER.debug("The straight fight between {} and {} has ended!", leftArmy.getSoldiersAndTheirHp(), rightArmy.getSoldiersAndTheirHp());
+        return leftArmy.isAlive();
     }
 }
