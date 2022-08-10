e38c0fb347668f1f62039eaa8c03dabacdf8084b
Jakub Dudek <jdude@softserveinc.com>
2022-08-10 12:29:37 +0200

    ArmyStraightTypeFightTestSuite new tests #NEW

--- src/test/java/org/study/warriors/model/ArmyStraightTypeFightTestSuite.java
+++ src/test/java/org/study/warriors/model/ArmyStraightTypeFightTestSuite.java
@@ -1,9 +1,34 @@
 package org.study.warriors.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.study.warriors.service.Battle;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArmyStraightTypeFightTestSuite {
 
-//    @Test
+    @Test
+    @DisplayName("1. Battlefield - StraightFight: x2 Warriors ATTACK vs. DEFEND x2 Warriors -> DEFENDERS should lose")
+    void test1() {
+        //Given
+        var attackers = new Army();
+        var defenders = new Army();
+        attackers.addUnits(new Lancer(), 7)
+                .addUnits(new Vampire(), 3)
+                .addUnits(new Warrior(), 4)
+                .addUnits(new Defender(), 2);
+
+        defenders.addUnits(new Warrior(), 4)
+                .addUnits(new Defender(),4 )
+                .addUnits(new Vampire(), 6)
+                .addUnits(new Lancer(), 4);
+
+        //When
+        var battleResult = Battle.straightFight(attackers, defenders);
+
+        //Then
+        assertTrue(battleResult);
+    }
 
 }
