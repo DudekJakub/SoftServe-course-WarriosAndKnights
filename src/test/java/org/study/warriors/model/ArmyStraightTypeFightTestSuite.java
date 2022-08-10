7eb754cb4169cbd3006b56c68fd8691b5864ea83
Jakub Dudek <jdude@softserveinc.com>
2022-08-10 13:33:52 +0200

    ArmyStraightTypeFightTestSuite new tests #UPDATE

--- src/test/java/org/study/warriors/model/ArmyStraightTypeFightTestSuite.java
+++ src/test/java/org/study/warriors/model/ArmyStraightTypeFightTestSuite.java
@@ -5,12 +5,35 @@ import org.junit.jupiter.api.Test;
 import org.study.warriors.service.Battle;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.study.warriors.model.interfaces.Unit.UnitType.LANCER;
 
 public class ArmyStraightTypeFightTestSuite {
 
     @Test
-    @DisplayName("1. Battlefield - StraightFight: x2 Warriors ATTACK vs. DEFEND x2 Warriors -> DEFENDERS should lose")
+    @DisplayName("1. Battlefield - StraightFight: ATTACKERS vs. DEFENDERS -> ATTACKERS should lose")
     void test1() {
+        //Given
+        var attackers = new Army();
+        var defenders = new Army();
+        attackers.addUnits(Lancer.class, 5)
+                .addUnits(Vampire.class, 3)
+                .addUnits(Warrior.class, 4)
+                .addUnits(Defender.class, 2);
+        defenders.addUnits(Warrior.class, 4)
+                .addUnits(Defender.class, 4)
+                .addUnits(Vampire.class, 6)
+                .addUnits(Lancer.class, 5);
+
+        //When
+        var battleResult = Battle.straightFight(attackers, defenders);
+
+        //Then
+        assertFalse(battleResult);
+    }
+
+    @Test
+    @DisplayName("2. Battlefield - StraightFight: ATTACKERS vs. DEFENDERS -> ATTACKERS should win")
+    void test2() {
         //Given
         var attackers = new Army();
         var defenders = new Army();
@@ -20,7 +43,7 @@ public class ArmyStraightTypeFightTestSuite {
                 .addUnits(new Defender(), 2);
 
         defenders.addUnits(new Warrior(), 4)
-                .addUnits(new Defender(),4 )
+                .addUnits(new Defender(),4)
                 .addUnits(new Vampire(), 6)
                 .addUnits(new Lancer(), 4);
 
@@ -31,4 +54,29 @@ public class ArmyStraightTypeFightTestSuite {
         assertTrue(battleResult);
     }
 
+    @Test
+    @DisplayName("3. Battlefield - StraightFight: ATTACKERS vs. DEFENDERS -> ATTACKERS should lose")
+    void test3() {
+        //Given
+        var attackers = new Army();
+        var defenders = new Army();
+        attackers.addUnits(new Lancer(), 7)
+                .addUnits(new Vampire(), 3)
+                .addUnits(new Healer(), 1)
+                .addUnits(new Warrior(), 4)
+                .addUnits(new Healer(), 1)
+                .addUnits(new Defender(),2);
+
+        defenders.addUnits(new Warrior(), 4)
+                .addUnits(new Defender(),4)
+                .addUnits(new Healer(), 1)
+                .addUnits(new Vampire(), 6)
+                .addUnits(new Lancer(), 4);
+
+        //When
+        var battleResult = Battle.straightFight(attackers, defenders);
+
+        //Then
+        assertFalse(battleResult);
+    }
 }
